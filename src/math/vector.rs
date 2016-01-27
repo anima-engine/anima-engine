7c53b08acd5bdb513e573a36e9bf4f3c18d4f10f
Dragos Tiselice <dragostiselice@gmail.com>
2016-01-27 15:19:18 +0200

    Added license to source and test files.

--- src/math/vector.rs
+++ src/math/vector.rs
@@ -1,3 +1,17 @@
+// Anima Engine  Copyright (C) 2015  Dragoș Tiselice
+// This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+// This is free software, and you are welcome to redistribute it
+// under certain conditions; type `show c' for details.
+//
+// The hypothetical commands `show w' and `show c' should show the appropriate
+// parts of the General Public License.  Of course, your program's commands
+// might be different; for a GUI interface, you would use an "about box".
+//
+// You should also get your employer (if you work as a programmer) or school,
+// if any, to sign a "copyright disclaimer" for the program, if necessary.
+// For more information on this, and how to apply and follow the GNU GPL, see
+// <http://www.gnu.org/licenses/>.
+
 /// A simple vector `struct` tailored specifically for graphics.
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Vector {
